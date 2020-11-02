94ea316e1000344ba46dc3bc7ac0e077df976271
nickmcmahon01 <nick.mcmahon@digital.justice.gov.uk>
2020-11-02 22:27:13 +0000

    Sql error.

--- src/main/java/uk/gov/justice/digital/hmpps/csr/api/repository/SqlRepository.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/csr/api/repository/SqlRepository.kt
@@ -170,54 +170,60 @@ class SqlRepository(private val jdbcTemplate: NamedParameterJdbcTemplate) {
 
         val GET_MODIFIED_DETAILS = """
             SELECT DISTINCT usr.name AS quantumId, 
-                            (SELECT MAX(tw_protocol.lastmodified)
+                            (
+                                SELECT MAX(tw_protocol.lastmodified)
                                 FROM tw_protocol 
                                 WHERE st_staff_id = sched.st_staff_id
                                 AND layer = -1 
-                                AND (tw_protocol.on_date BETWEEN (SYSDATE - 1) AND (SYSDATE + 1))) as shiftModified,
+                                AND (tw_protocol.on_date BETWEEN (SYSDATE - 1) AND (SYSDATE + 1))
+                            ) as shiftModified,
                             sched.on_date as date, 
                             CASE sched.level_id WHEN 4000 THEN 1 ELSE 0 END AS shiftType,
                             sched.task_start as startTime, 
                             sched.task_end as endTime, 
                             DECODE (tk_model.name, NULL, tk_type.name, tk_model.name) as activity
             FROM tw_schedule sched 
+            
                 INNER JOIN sm_user usr ON sched.st_staff_id = usr.obj_id AND usr.obj_type = 3 AND usr.is_deleted = 0 
                 LEFT JOIN tk_type ON sched.ref_id = tk_type.tk_type_id 
                 LEFT JOIN tk_model ON tk_model.tk_model_id = sched.optional_1 
-                        -- Filter by staff that are currently assigned to Planning Unit 
-            WHERE sched.st_staff_id IN 
-                (SELECT st_staff.st_staff_id 
+                
+            -- Filter by staff that are currently assigned to Planning Unit 
+            WHERE sched.st_staff_id IN (
+                    SELECT st_staff.st_staff_id 
                     FROM st_staff 
-                        INNER JOIN st_planunit ON st_staff.st_staff_id = st_planunit.st_staff_id 
+                    INNER JOIN st_planunit ON st_staff.st_staff_id = st_planunit.st_staff_id 
                     -- Staff belonging to all children Planning Unit IDs (no need for a JOIN!) 
                     -- There are duplicated recs for some PUs, so use is_deleted=0 to search for active ones only! 
-                    WHERE st_planunit.pu_planunit_id IN
-                        (SELECT pu_planunit_id 
-                            FROM pu_planunit 
-                            WHERE is_deleted = 0 
-                            AND lower(pu_planunit.name) NOT LIKE '%virtual%' 
-                            AND lower(pu_planunit.name) LIKE LOWER(:planUnit) || '%' ) 
-                            -- Staff assignment must be valid prior to current date (exclude date = 01-JAN-00 because 00 = 1900!) 
-                        AND (FLOOR(st_planunit.valid_from - (SYSDATE - 1)) <= 0 
-                            AND TO_CHAR(st_planunit.valid_from,'YY') > 0) 
-                        AND st_planunit.valid_to > SYSDATE 
-                        AND st_planunit.priority = 1 
-                        AND st_staff.is_deleted = 0) 
-                    AND sched.pu_planunit_id IN 
-                        (SELECT pu_planunit_id 
-                            FROM pu_planunit 
-                            WHERE is_deleted = 0 
-                            AND lower(pu_planunit.name) NOT LIKE '%virtual%' 
-                            AND lower(pu_planunit.name) LIKE lower(:planUnit) || '%' ) 
-                    AND sched.LAYER = -1 -- TOP LAYER 
-                    AND sched.level_id IN(1000, 4000) -- detail and time recording lines 
-                    AND sched.sched_lastmodified >= (SYSDATE - 1)
-                    AND sched.on_date <= (SYSDATE + 1)
-                        AND (
+                    WHERE st_planunit.pu_planunit_id IN (
+                                                            SELECT pu_planunit_id 
+                                                            FROM pu_planunit 
+                                                            WHERE is_deleted = 0 
+                                                            AND lower(pu_planunit.name) NOT LIKE '%virtual%' 
+                                                            AND lower(pu_planunit.name) LIKE LOWER(:planUnit) || '%' 
+                                                         ) 
+                                        -- Staff assignment must be valid prior to current date (exclude date = 01-JAN-00 because 00 = 1900!) 
+                                        AND (FLOOR(st_planunit.valid_from - (SYSDATE - 1)) <= 0 AND TO_CHAR(st_planunit.valid_from,'YY') > 0) 
+                                        AND st_planunit.valid_to > SYSDATE
+                         
+                                        AND st_planunit.priority = 1 
+                                        AND st_staff.is_deleted = 0
+                                        ) 
+                AND sched.pu_planunit_id IN (
+                                                SELECT pu_planunit_id 
+                                                FROM pu_planunit 
+                                                WHERE is_deleted = 0 
+                                                AND lower(pu_planunit.name) NOT LIKE '%virtual%' 
+                                                AND lower(pu_planunit.name) LIKE lower(:planUnit) || '%' ) 
+
+                                            )
+                AND sched.on_date <= (SYSDATE + 1)
+                AND (
+                        (
                             -- Check timings for current day 
                             TO_NUMBER(TRUNC((SYSDATE + 1) - sched.on_date) * (86400 / 3600)) = (86400 / 3600)
                             -- Tasks that have not yet started 
-                            AND (TO_NUMBER(ROUND(sched.task_start/3600, 0)) >= TO_NUMBER(TO_CHAR(SYSDATE, 'HH24'))
+                            AND (TO_NUMBER(ROUND(sched.task_start/3600, 0)) >= TO_NUMBER(TO_CHAR(SYSDATE, 'HH24')))
                         )
                         OR ( 
                             -- Check timings for the following day 
@@ -225,7 +231,12 @@ class SqlRepository(private val jdbcTemplate: NamedParameterJdbcTemplate) {
                             -- task start time must be within x hrs from now
                             AND TO_NUMBER(ROUND(sched.task_start/3600, 0)) <= TO_NUMBER(TO_CHAR(SYSDATE, 'HH24'))
                         )
-                )""".trimIndent()
+                    )
+                AND sched.sched_lastmodified >= (SYSDATE - 1)
+
+                AND sched.LAYER = -1 -- TOP LAYER
+                AND sched.level_id IN(1000, 4000) -- detail and time recording lines
+                """.trimIndent()
 
         val GET_DETAIL_TEMPLATES = """
             SELECT tk_modelItem.TASK_START AS startTime,
