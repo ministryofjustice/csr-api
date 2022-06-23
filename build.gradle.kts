7e45558fae87ff5f9a7edcc339e6b6f9cadd5323
petergphillips <peter@greenthistle.co.uk>
2022-06-23 09:11:30 +0100

    SDI-252: ⬆ Try out jdk 18 and kotlin 1.7 (#78)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.2.3"
-  kotlin("plugin.spring") version "1.6.21"
-  kotlin("plugin.jpa") version "1.6.21"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.3.0-beta"
+  kotlin("plugin.spring") version "1.7.0"
+  kotlin("plugin.jpa") version "1.7.0"
   idea
 }
 
@@ -22,7 +22,7 @@ dependencies {
 
   runtimeOnly("com.h2database:h2:2.1.212")
   runtimeOnly("com.zaxxer:HikariCP")
-  runtimeOnly("org.flywaydb:flyway-core:8.5.12")
+  runtimeOnly("org.flywaydb:flyway-core:8.5.13")
   runtimeOnly("com.oracle.database.jdbc:ojdbc10:19.15.0.0.1")
 
   implementation("org.springframework.boot:spring-boot-starter-jdbc")
@@ -58,11 +58,11 @@ dependencies {
 
 // Language versions
 java {
-  toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+  toolchain.languageVersion.set(JavaLanguageVersion.of(18))
 }
 
 tasks {
   compileKotlin {
-    kotlinOptions.jvmTarget = "17"
+    kotlinOptions.jvmTarget = "18"
   }
 }
