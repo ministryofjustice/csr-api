cd03425a27f5dbecb42bc83c6e9ae86a3bb85f47
petergphillips <peter@greenthistle.co.uk>
2023-06-22 08:34:22 +0100

    SDIT-893: ✨ Cache jwks indefinitely (#135)

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+  implementation("org.springframework.boot:spring-boot-starter-cache")
 
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
 
