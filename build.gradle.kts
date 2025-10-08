aa186e06a6c9933729ef8124aad7572b258b3049
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-10-08 10:14:16 +0100

    SDIT-3072: ✨ Upgrade to java 25

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,10 @@ plugins {
 }
 
 configurations {
-  implementation { exclude(mapOf("module" to "tomcat-jdbc")) }
+  implementation {
+    exclude(module = "commons-logging")
+    exclude(module = "tomcat-jdbc")
+  }
 }
 
 dependencies {
@@ -43,14 +46,18 @@ dependencies {
 
 // Language versions
 kotlin {
-  jvmToolchain(21)
   compilerOptions {
     freeCompilerArgs.addAll("-Xjvm-default=all", "-Xwhen-guards", "-Xannotation-default-target=param-property")
   }
 }
 
+java {
+  sourceCompatibility = JavaVersion.VERSION_24
+  targetCompatibility = JavaVersion.VERSION_24
+}
+
 tasks {
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    compilerOptions.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
+    compilerOptions.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_24
   }
 }
