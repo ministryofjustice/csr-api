c81dcb4ec1b7bc41bd3942e29a448a1534ed237b
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2026-04-23 11:25:49 +0100

    SDIT-3674: ♻️  Switch to single database connection rather than per region

--- src/main/java/uk/gov/justice/digital/hmpps/csr/api/config/RegionAwareRoutingSource.kt
+++
@@ -1,9 +0,0 @@
-package uk.gov.justice.digital.hmpps.csr.api.config
-
-import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource
-import uk.gov.justice.digital.hmpps.csr.api.utils.RegionContext
-
-class RegionAwareRoutingSource : AbstractRoutingDataSource() {
-
-  override fun determineCurrentLookupKey(): Any = RegionContext.getRegion().toString()
-}
